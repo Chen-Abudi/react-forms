773a94650f0e90e625eed8e0e34fb0194c019f6e
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-04-28 18:26:22 +0300

    feat: Validate input upon lost focus (blur)

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -9,6 +9,13 @@ export default function Login() {
     password: "",
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+
   function handleFormSubmit(evt) {
     evt.preventDefault();
 
@@ -19,6 +26,17 @@ export default function Login() {
     setEnteredValues((prevValues) => ({
       ...prevValues,
       [identifier]: value,
+    }));
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
     }));
   }
 
@@ -41,9 +59,13 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             onChange={(evt) => handleInputChange("email", evt.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
