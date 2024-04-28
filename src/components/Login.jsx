034344025663ad996085f2787f10f83912e713bb
Grace Chen Abudi <cheezumcali23@gmail.com>
2024-04-28 13:25:15 +0300

    feat: Implement use input management via state and generic handlers

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
   function handleFormSubmit(evt) {
     evt.preventDefault();
-    console.log("Submitted!");
+
+    console.log(enteredValues);
   }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+  }
+
+  // function handleEmailChange(evt) {
+  //   setEnteredEmail(evt.target.value);
+  // }
+
+  // function handlePasswordChange(evt) {
+  //   setEnteredPassword(evt.target.value);
+  // }
 
   return (
     <form onSubmit={handleFormSubmit}>
@@ -11,12 +37,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(evt) => handleInputChange("email", evt.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(evt) => handleInputChange("password", evt.target.value)}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
